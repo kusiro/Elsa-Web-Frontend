7cd62b23e4aa11fc5b4caae6f9bb65522575b431
terry623 <terry92516@gmail.com>
2019-01-03 00:05:47 +0800

    finish mobile project page

--- src/terry/components/Projects/index.js
+++ src/terry/components/Projects/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import MediaQuery from 'react-responsive';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Col, Row } from 'antd';
 import { Link } from 'react-router';
@@ -17,14 +18,34 @@ import {
   LogoContent,
   MainRow,
   MedContent,
+  PageLink,
   SmallContent,
+  Text,
+  TextCol,
   Title1,
   Title2,
   TitleText,
 } from '../Share';
+import { media, notebook } from '../size';
 
 const Blocks = styled.div`
   padding-top: 20vh;
+
+  ${media.lessThan('notebook')`
+    padding-top: 0;
+  `};
+`;
+
+const BackgroundStyleColor = styled(BackgroundColor)`
+  ${media.lessThan('notebook')`
+    height: 60vh;
+  `};
+`;
+
+const BackgroundStyleColor2 = styled(BackgroundColor)`
+  ${media.lessThan('notebook')`
+    height: 100%;
+  `};
 `;
 
 const EachBlock = styled.div`
@@ -34,20 +55,36 @@ const EachBlock = styled.div`
   margin-bottom: 5vh;
   color: white;
   font-size: 1.2vw;
+
+  ${media.lessThan('notebook')`
+    margin-bottom: 0;
+  `};
 `;
 
 const Year = styled.div`
   margin-bottom: -1vh;
+  font-weight: bold;
 `;
 
 const Title = styled.div`
   font-size: 2vw;
+
+  ${media.lessThan('notebook')`
+    padding-top: 1vh;
+    font-size:5vw;
+  `};
 `;
 
 const TextArea = styled.div`
   padding-left: 2.5vw;
   padding-right: 4vw;
   padding-top: 2vh;
+
+  ${media.lessThan('notebook')`
+    padding-top: 3.5vh;
+    padding-left: 4vw;
+    font-size:2vw;
+  `};
 `;
 
 const ImageArea = styled.div`
@@ -56,6 +93,18 @@ const ImageArea = styled.div`
   background: url(${props => props.image});
   background-size: cover;
   background-position: center center;
+`;
+
+const TitleStyleText = styled(TitleText)`
+  ${media.lessThan('notebook')`
+    font-size: 10vw;
+  `};
+`;
+
+const IconStyleImage = styled(IconImage)`
+  ${media.lessThan('notebook')`
+    width: 8vw;
+  `};
 `;
 
 const ProjectsContent = [
@@ -71,66 +120,121 @@ const ProjectsContent = [
   },
 ];
 
-const Projects = () => (
-  <Row>
-    <Col span={9}>
-      <BackgroundColor color="#a1afd8">
-        <MainRow type="flex" justify="center">
-          <LogoContent span={18}>
-            <Row type="flex" justify="start" align="middle" gutter={8}>
-              <Col>
-                <IconImage src={IconImg} />
+class Projects extends Component {
+  renderProject = () => {
+    let isChangeOrder = false;
+    return ProjectsContent.map(({ content, link, image }) => {
+      isChangeOrder = !isChangeOrder;
+      return (
+        <Link key={content[1]} to={link}>
+          <EachBlock key={content[1]}>
+            <Row type="flex">
+              <Col
+                span={12}
+                xs={{ order: isChangeOrder ? 2 : 1 }}
+                xl={{ order: 1 }}
+              >
+                <TextArea>
+                  <Year>{content[0]}</Year>
+                  <Title>{content[1]}</Title>
+                  <p>{content[2]}</p>
+                </TextArea>
               </Col>
-              <Col>
-                <Title1>NTHU</Title1>
-                <Title2>ELSA</Title2>
+              <Col
+                span={12}
+                xs={{ order: isChangeOrder ? 1 : 2 }}
+                xl={{ order: 2 }}
+              >
+                <ImageArea image={image} />
               </Col>
             </Row>
-          </LogoContent>
-          <SmallContent span={18} color="#8c8c8c">
-            <Row type="flex" justify="start" align="bottom">
-              <Col span={6}>
-                <Hr color="#8c8c8c" />
-              </Col>
-              <Col span={12} offset={1}>
-                Home
-              </Col>
-            </Row>
-          </SmallContent>
-          <BigTitle span={18}>
-            <TitleText>Projects</TitleText>
-          </BigTitle>
-          <MedContent span={12} color="#8c8c8c" />
-          <Col span={6} />
-        </MainRow>
-      </BackgroundColor>
-    </Col>
-    <Col span={15}>
-      <BackgroundColor color="white">
-        <Header fontColor="#9b9b9b" />
-        <Blocks>
-          {ProjectsContent.map(({ content, link, image }) => (
-            <Link key={content[1]} to={link}>
-              <EachBlock key={content[1]}>
-                <Row>
-                  <Col span={12}>
-                    <TextArea>
-                      <Year>{content[0]}</Year>
-                      <Title>{content[1]}</Title>
-                      <p>{content[2]}</p>
-                    </TextArea>
+          </EachBlock>
+        </Link>
+      );
+    });
+  };
+
+  renderLogin = () => {
+    const { token } = localStorage;
+    if (token) {
+      return (
+        <PageLink to="/logout">
+          <Text color="rgba(0, 0, 0, 0.4)">Sign out</Text>
+        </PageLink>
+      );
+    }
+    return (
+      <PageLink to="/login">
+        <Text color="rgba(0, 0, 0, 0.4)">Sign in</Text>
+      </PageLink>
+    );
+  };
+
+  renderOtherBlock = () => (
+    <Row>
+      <TextCol span={24}>
+        <PageLink to="/about">
+          <Text color="rgba(0, 0, 0, 0.4)">About Elsa Lab</Text>
+        </PageLink>
+      </TextCol>
+      <TextCol span={24}>{this.renderLogin()}</TextCol>
+    </Row>
+  );
+
+  render() {
+    return (
+      <Row>
+        <Col xs={{ span: 24 }} xl={{ span: 9 }}>
+          <BackgroundStyleColor color="#a1afd8">
+            <MainRow type="flex" justify="center">
+              <LogoContent xs={{ span: 22 }} xl={{ span: 18 }}>
+                <Row type="flex" justify="start" align="middle" gutter={8}>
+                  <Col>
+                    <IconStyleImage src={IconImg} />
                   </Col>
-                  <Col span={12}>
-                    <ImageArea image={image} />
+                  <Col>
+                    <Title1>NTHU</Title1>
+                    <Title2>ELSA</Title2>
+                  </Col>
+                  <Col xs={{ span: 10 }} xl={{ span: 0 }} offset={8}>
+                    {this.renderOtherBlock()}
                   </Col>
                 </Row>
-              </EachBlock>
-            </Link>
-          ))}
-        </Blocks>
-      </BackgroundColor>
-    </Col>
-  </Row>
-);
+              </LogoContent>
+              <SmallContent xs={{ span: 22 }} xl={{ span: 18 }} color="#8c8c8c">
+                <Row type="flex" justify="start" align="bottom">
+                  <Col span={6}>
+                    <Hr color="#8c8c8c" />
+                  </Col>
+                  <Col span={12} offset={1}>
+                    Home
+                  </Col>
+                </Row>
+              </SmallContent>
+              <BigTitle xs={{ span: 20 }} xl={{ span: 18 }}>
+                <TitleStyleText>Projects</TitleStyleText>
+              </BigTitle>
+              <MedContent span={12} color="#8c8c8c" />
+              <Col span={6} />
+            </MainRow>
+          </BackgroundStyleColor>
+        </Col>
+        <Col xs={{ span: 24 }} xl={{ span: 15 }}>
+          <BackgroundStyleColor2 color="white">
+            <MediaQuery query={`(max-width: ${notebook})`}>
+              {matches => {
+                if (!matches) {
+                  return <Header fontColor="#9b9b9b" />;
+                }
+                return <></>;
+              }}
+            </MediaQuery>
+            <Blocks>{this.renderProject()}</Blocks>
+          </BackgroundStyleColor2>
+        </Col>
+      </Row>
+    );
+  }
+}
 
 export default Projects;
