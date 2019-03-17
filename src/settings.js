9ee785ec1d6f80b2436cc0f851e75e1edd595a40
terry623 <terry92516@gmail.com>
2019-03-17 18:13:17 +0800

    url

--- src/settings.js
+++ src/settings.js
@@ -1,10 +1,10 @@
 const settings = {
     // root_url: 'http://elsa.cs.nthu.edu.tw',
-    // backend_url: 'http://140.114.75.146:8000',
-    // root_url: 'http://127.0.0.1:3000',
-    // backend_url: 'http://127.0.0.1:8000',
-    root_url: 'https://elsa-lab.appspot.com/',
-    backend_url: 'https://elsa-backend.appspot.com/',
+    // backend_url: 'http://192.168.99.100:3000',
+    root_url: 'http://127.0.0.1:3000',
+    backend_url: 'http://127.0.0.1:8000',
+    // root_url: 'https://elsa-lab.appspot.com/',
+    // backend_url: 'https://elsa-backend.appspot.com/',
     root_user_types: ['College', 'Master', 'PHD', 'Teacher'],
     all_user_types: ['course_student', 'College', 'Master', 'PHD', 'Teacher', 'Alumni'],
 }
