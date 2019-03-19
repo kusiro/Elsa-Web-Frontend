a8ff81bf022e70c8951603baf846ef1e3a11c582
terry623 <terry92516@gmail.com>
2019-03-19 22:42:46 +0800

    change setting url

--- src/settings.js
+++ src/settings.js
@@ -1,10 +1,9 @@
 const settings = {
-    // root_url: 'http://elsa.cs.nthu.edu.tw',
-    // backend_url: 'http://192.168.99.100:3000',
-    root_url: 'http://127.0.0.1:3000',
-    backend_url: 'http://127.0.0.1:8000',
-    // root_url: 'https://elsa-lab.appspot.com/',
-    // backend_url: 'https://elsa-backend.appspot.com/',
+    // root_url: 'http://127.0.0.1:3000',
+    root_url: 'https://elsa-lab.appspot.com',
+    // backend_url: 'http://127.0.0.1:8080',
+    // backend_url: 'http://192.168.99.100:8080',
+    backend_url: 'https://elsa-backend.appspot.com',
     root_user_types: ['College', 'Master', 'PHD', 'Teacher'],
     all_user_types: ['course_student', 'College', 'Master', 'PHD', 'Teacher', 'Alumni'],
 }
