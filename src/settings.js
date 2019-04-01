5e4ade6a1082e9e0d0b8ccdbb83379f0c86a7c44
terry623 <terry92516@gmail.com>
2019-04-01 17:44:29 +0800

    update new url

--- src/settings.js
+++ src/settings.js
@@ -2,8 +2,7 @@ const settings = {
     // root_url: 'http://127.0.0.1:3000',
     root_url: 'https://elsa-lab.appspot.com',
     // backend_url: 'http://127.0.0.1:8080',
-    // backend_url: 'http://192.168.99.100:8080',
-    backend_url: 'https://elsa-backend.appspot.com',
+    backend_url: 'http://35.201.173.113:8080',
     root_user_types: ['College', 'Master', 'PHD', 'Teacher'],
     all_user_types: ['course_student', 'College', 'Master', 'PHD', 'Teacher', 'Alumni'],
 }
