3ee7cecfeeb607eca20fd772c954a23e46a05bac
Terry Lin <terry92516@gmail.com>
2019-05-19 17:16:48 +0800

    fix eslint ( homepage )

--- src/Management/HomePage/CoverImage/Index/Index.js
+++ src/Management/HomePage/CoverImage/Index/Index.js
@@ -1,48 +1,47 @@
 import https from 'https';
 
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 import axios from 'axios';
-import './Index.css'
-import settings from '../../../../settings.js'
+
+import settings from '../../../../settings';
+
+import './Index.css';
 
 class CoverImageIndex extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            coverImageUrls: []
-        }
-    }
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       coverImageUrls: [],
+  //     };
+  //   }
 
-    componentWillMount() {
-        const ins = axios.create({
-            baseURL: settings.backend_url,
-            timeout: 1000,
-            headers: {
-                Authorization: "JWT " + token,
-            },
-            httpsAgent: new https.Agent({
-                rejectUnauthorized: false,
-            }),
-        })
+  componentWillMount() {
+    const { token } = localStorage;
+    const ins = axios.create({
+      baseURL: settings.backend_url,
+      timeout: 1000,
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+    });
 
-        ins.get('home_cover_image')
-        .then((res) => {
-            console.log(res);
-            this.setState({news: res.data})
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }
+    ins
+      .get('home_cover_image')
+      .then(res => {
+        console.log(res);
+        // this.setState({ news: res.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    render() {
-        return(
-            <div>
-                coverimage
-            </div>
-        )
-    }
+  render() {
+    return <div>coverimage</div>;
+  }
 }
 
 export default CoverImageIndex;
