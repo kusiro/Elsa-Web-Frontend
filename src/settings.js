2b24ef65736d365d21c50203dc9f93eff9b31666
Terry Lin <terry92516@gmail.com>
2019-05-28 16:44:41 +0800

    new setting url

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,8 @@
 // FIXME: root_url 的 Port 若要改，static server 的 Port 也要改
 const settings = {
-  root_url: 'http://127.0.0.1:3000',
+  // root_url: 'http://127.0.0.1:3000',
   // root_url: 'https://elsa-lab.appspot.com',
-  // root_url: 'http://35.229.221.5:8080',
+  root_url: 'http://35.229.221.5:8080',
   // backend_url: 'http://127.0.0.1:8080',
   backend_url: 'http://35.201.173.113:8080',
   root_user_types: ['College', 'Master', 'PHD', 'Teacher'],
