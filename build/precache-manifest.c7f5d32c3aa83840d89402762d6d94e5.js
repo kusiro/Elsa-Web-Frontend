4bddf3454ae12273bba4a62de922c0f08258f848
Terry Lin <terry92516@gmail.com>
2019-06-02 14:57:03 +0800

    remove build folder from .gitignore

---
+++ build/precache-manifest.c7f5d32c3aa83840d89402762d6d94e5.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = [
+  {
+    "revision": "407ee0caf6acc584af4d872adba80551",
+    "url": "/static/media/Method1.407ee0ca.jpg"
+  },
+  {
+    "revision": "db816b0187c23bee2686",
+    "url": "/static/css/main.58088ca9.chunk.css"
+  },
+  {
+    "revision": "d28869ef4c530535c8d4e5b7a0924a8e",
+    "url": "/static/media/add.d28869ef.svg"
+  },
+  {
+    "revision": "83af9090ac658f4cc172",
+    "url": "/static/js/1.83af9090.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d248ec3c3d6a82bb77ab3af1acbcc83",
+    "url": "/static/media/icon.3d248ec3.png"
+  },
+  {
+    "revision": "8a2fcd5dc8c66070c084b416247977d4",
+    "url": "/static/media/background_image_green.8a2fcd5d.jpg"
+  },
+  {
+    "revision": "026d253e8934c56351f7ab9066dfe8b7",
+    "url": "/static/media/background_image_purple.026d253e.jpg"
+  },
+  {
+    "revision": "701bac69f32e3384edbf5a38d51044b7",
+    "url": "/static/media/teacher.701bac69.png"
+  },
+  {
+    "revision": "87765955e96faba0ac35ad65d9b91fc9",
+    "url": "/static/media/background_image_invert_vertical.87765955.jpg"
+  },
+  {
+    "revision": "dc606291ddb7d3f7cb472c116a1f1edb",
+    "url": "/static/media/background_image_invert.dc606291.jpg"
+  },
+  {
+    "revision": "a33652557eef856e9a412ca9818431e3",
+    "url": "/static/media/Courses.a3365255.jpg"
+  },
+  {
+    "revision": "f57d3e4953c0172055db36e1de5584aa",
+    "url": "/static/media/News.f57d3e49.jpg"
+  },
+  {
+    "revision": "86ac89da411cd3bee9c8c8047d7ea519",
+    "url": "/static/media/Projects.86ac89da.jpg"
+  },
+  {
+    "revision": "c6b5f0c78a01758be4e81d40ebc82a0f",
+    "url": "/static/media/Publications.c6b5f0c7.jpg"
+  },
+  {
+    "revision": "c4310724b4eebb704cdcf24a8b8ba750",
+    "url": "/static/media/delta.c4310724.jpg"
+  },
+  {
+    "revision": "bd0e09505b4f517d79aebb991399cfbe",
+    "url": "/static/media/background_image_invert_vertical_2.bd0e0950.jpg"
+  },
+  {
+    "revision": "19bda8d8ed39b16fba8b7345afd417a7",
+    "url": "/static/media/background_image.19bda8d8.jpg"
+  },
+  {
+    "revision": "a7b3caf70904e0a74081dbdfa5d84859",
+    "url": "/static/media/award_1.a7b3caf7.png"
+  },
+  {
+    "revision": "24d32c5afad4f302d44544e0dcfcc300",
+    "url": "/static/media/award_2.24d32c5a.png"
+  },
+  {
+    "revision": "01c0ae5cb32424f771624f565a8d6815",
+    "url": "/static/media/Method2.01c0ae5c.png"
+  },
+  {
+    "revision": "6a33052b1aec1dc0b47cc6a49822720e",
+    "url": "/static/media/Result1.6a33052b.png"
+  },
+  {
+    "revision": "6e6eff1c09fc39f261ac0b26b03dc9f3",
+    "url": "/static/media/Result2.6e6eff1c.png"
+  },
+  {
+    "revision": "f2188b0bcf3e331f9c7e37aa2c6ee892",
+    "url": "/static/media/Result3.f2188b0b.png"
+  },
+  {
+    "revision": "db816b0187c23bee2686",
+    "url": "/static/js/main.db816b01.chunk.js"
+  },
+  {
+    "revision": "2e2d01f993d8f1ae22ed6eb615904d90",
+    "url": "/static/media/Method2.2e2d01f9.jpg"
+  },
+  {
+    "revision": "6cbb65400e92d0b1306bd5ca077c91a7",
+    "url": "/static/media/Method3.6cbb6540.jpg"
+  },
+  {
+    "revision": "845887dec634c1d4b9c0757c2bb3743f",
+    "url": "/static/media/Result11.845887de.jpg"
+  },
+  {
+    "revision": "d571e150d7be9a20b6070b0f982efa82",
+    "url": "/static/media/Result12.d571e150.jpg"
+  },
+  {
+    "revision": "edc36c3ab0233b2b2e7788ed4dd0cf4b",
+    "url": "/static/media/Result2.edc36c3a.jpg"
+  },
+  {
+    "revision": "e986b7ace2d1caba4993cea1024ac97e",
+    "url": "/static/media/Result31.e986b7ac.jpg"
+  },
+  {
+    "revision": "1aab7591bedfe41a5047d83d0421c35f",
+    "url": "/static/media/Result32.1aab7591.jpg"
+  },
+  {
+    "revision": "ba933434a088a3a26b3b288f68b2ab45",
+    "url": "/static/media/Result41.ba933434.jpg"
+  },
+  {
+    "revision": "7f26179d6e721297f653d03ac40a1f2a",
+    "url": "/static/media/Result42.7f26179d.jpg"
+  },
+  {
+    "revision": "33c135eef309170f75be3d1dfc93dc2b",
+    "url": "/static/media/Method1.33c135ee.png"
+  },
+  {
+    "revision": "3acffa2ef2baecbe07d0f61c52e59ce2",
+    "url": "/static/media/Method2.3acffa2e.png"
+  },
+  {
+    "revision": "e65933c13fe1e014f2300a43592ce4d6",
+    "url": "/static/media/Method31.e65933c1.png"
+  },
+  {
+    "revision": "36c347d320697d8e00eec713ad0e90ad",
+    "url": "/static/media/Method32.36c347d3.png"
+  },
+  {
+    "revision": "026d253e8934c56351f7ab9066dfe8b7",
+    "url": "/static/media/background_image_footer.026d253e.jpg"
+  },
+  {
+    "revision": "99899a4a89afa46429f2859f144a1977",
+    "url": "/static/media/System_structure_img_purple.99899a4a.png"
+  },
+  {
+    "revision": "0d53515e3236113c74db3e0391c0c438",
+    "url": "/static/media/System_structure_img_green.0d53515e.png"
+  },
+  {
+    "revision": "6f7712a6e666441841013f348f7d3376",
+    "url": "/static/media/System_structure_img.6f7712a6.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "83af9090ac658f4cc172",
+    "url": "/static/css/1.b0d0b936.chunk.css"
+  },
+  {
+    "revision": "3c88b73e5b217bda0c9d02102c7ff77f",
+    "url": "/index.html"
+  }
+];
